9e19db29842b05ad88c04c322b655a5d6f69f37e
eldi <eldi456.rangel@gmail.com>
2024-10-30 17:45:51 -0700

    fase 3

--- practica1.cpp
+++ practica1.cpp
@@ -5,8 +5,16 @@ using namespace std;
 
 int main(){
 
+char nombre [50];
+char correo [50];
+int cedula, n;
+cout<< "ingrese la cantidad de registros";
+cin>>n;
+cin.ignore();
+
+ofstream archivo("archivo.txt");
+if (archivo.is_open())
 
 
 
-    
 }
